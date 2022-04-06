1ba0463965ec0d459edd2825a6e88110e13d0002
dhamansehdev <38543939+dhamansehdev@users.noreply.github.com>
2022-04-06 09:26:16 -0500

    new

---
+++ src/basics/if_else_python.py
@@ -0,0 +1,22 @@
+print("This script basically provide simple example of if and else statements in python")
+
+# how to declare a variable
+value1 = 10
+value2 = 2
+
+print("Value1 is : {}".format(value1))
+
+print("Value2 is : {}".format(value2))
+
+
+# example of if and else  statement
+if value1 > value2:
+    print("{} is greater than {}".format(value1,value2))
+else:
+    print("{} is not greater than {}".format(value1,value2))
+
+
+"""
+You can add a multi line comment like this:
+* You can use if statement or if and else statement like above.
+"""
